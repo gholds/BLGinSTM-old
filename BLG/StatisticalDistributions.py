ab715fb1375f610b93e68421bed67ca18023ce45
gholds <gregoryholdman@gmail.com>
2018-07-14 09:25:49 -0400

    Removed folder BLG which ought not be referred to anymore

--- BLG/StatisticalDistributions.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-kB = 1.38064852 * 10**-23 # Boltmann constant J/K
-
-
-def FermiDirac(e,T):
-    '''Returns the Fermi-Dirac distribution.'''
-    # Using 1 / (np.exp()+1) introduces overflow errors. This keeps the floats of managable size.
-    return np.exp(-np.logaddexp(e/(kB*(T+0.0001)),0))
