22d94c2bb9dfa9de24f4e7bef651e127ae8bb30f
gholds <gregoryholdman@gmail.com>
2018-07-15 16:18:06 -0500

    Generated test currents

--- TEST.py
+++ TEST.py
@@ -11,5 +11,5 @@ experiment = TunnelingExperiments.BLGinSTM(d1,d2,e1,e2,T,W)
 VT = np.linspace(-1,1, num=10)
 VB = np.linspace(-45,45,num=10)
 
-vs = experiment.generate_vplus_vminus([VT[0],VT[-1]],0.05,[VB[0],VB[-1]],0.05,method='DasSarma')
-print(vs[0])
+tc = experiment.generate_tunnelcurrent([VT[0],VT[-1]],0.05,[VB[0],VB[-1]],0.05,method='DasSarma')
+print(experiment.I)
