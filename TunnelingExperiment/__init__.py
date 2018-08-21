b606232d727e3ed6eae981462884ef9ff9b9c00d
gholds <gregoryholdman@gmail.com>
2018-08-20 21:55:19 -0500

    Deleted a UniversalConstants file and moved its contents to STM.py

--- TunnelingExperiment/__init__.py
+++ TunnelingExperiment/__init__.py
@@ -2,10 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import matplotlib.animation as animation
-#from matplotlib.patches import ConnectionPatch
+
 from scipy import integrate, optimize
-#from StatisticalDistributions import Temperature
-from UniversalConstants import *
-#from Materials import Graphene
+
 import os
 from abc import ABCMeta # For inheritance
